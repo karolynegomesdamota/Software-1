e3151f612809a635137c44275c59effdc37ebb0a
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-21 22:51:11 +0300

    MOD 03, EX04

---
+++ Python/Module 03/exercise4.py
@@ -0,0 +1,17 @@
+first_integer_srt = input ("Enter the first integer: ")
+second_integer_srt = input ("Enter the second integer: ")
+third_integer_srt = input ("Enter the third integer: ")
+
+num1 = int(first_integer_srt)
+num2 = int(second_integer_srt)
+num3 = int(third_integer_srt)
+
+sum_of_numbers = num1 + num2 + num3
+product_of_numbers = num1 * num2 * num3
+average_of_numbers = (num1 + num2 + num3)/3
+
+print ("The sum of the numbers: " + str(sum_of_numbers))
+print ("The product of the numbers: " + str(product_of_numbers))
+print ("The average of the numbers: " + str(average_of_numbers))
+
+# Done independently. According to Moodle it is correct. Pending to confirm in class if it's correct.
