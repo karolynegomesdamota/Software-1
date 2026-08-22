c8539e569244ba06d1c7029b7ae1c844b28c5600
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-22 22:23:07 +0300

    MOD03-EX5 done (Note: Moodle giving error)

---
+++ Python/Module 03/exercise5.py
@@ -0,0 +1,17 @@
+talents = input ("Enter talents: ")
+pounds = input ("Enter pounds: ")
+lots = input ("Enter lots: ")
+
+talent = 20 * 32 * 13.3 * float(talents)
+pound = 32 * 13.3 * float(pounds)
+lot = 13.3 * float(lots)
+
+total_grams= lot + pound + talent
+kilograms= total_grams / 1000
+remaining_grams= (kilograms - int(kilograms)) * 1000
+
+print ("The weight in modern units:")
+print (str(int(kilograms)) + f" kilograms and {remaining_grams:.2f} grams.")
+
+# Note to myself: Remember input will always be a string so you must convert it to make calculations. And then, when printing, if mixed with strings, you must covert it back.
+# ASK IN CLASS ANYWAY - Note to myself: The output formatting does not work directly if you try to apply it to a str(remaining_grams), you must add it within a string and {remaining_grams:.2f}.
