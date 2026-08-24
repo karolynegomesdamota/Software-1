1bd2cc7378f46ac54bf197ab9a41e8fd54e81de6
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-24 13:42:45 +0300

    Adding a note about formatting

--- Python/Module 03/exercise5.py
+++ Python/Module 03/exercise5.py
@@ -15,3 +15,4 @@ print (str(int(kilograms)) + f" kilograms and {remaining_grams:.2f} grams.")
 
 # Note to myself: Remember input will always be a string so you must convert it to make calculations. And then, when printing, if mixed with strings, you must covert it back.
 # ASK IN CLASS ANYWAY - Note to myself: The output formatting does not work directly if you try to apply it to a str(remaining_grams), you must add it within a string and {remaining_grams:.2f}.
+# print (f"{remaining_grams:.2f}") works
