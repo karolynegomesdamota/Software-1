6311d41569d395e794b674edabe3d4c7d5be7503
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-25 14:17:30 +0300

    MOD03 - EX05 fixed (added float at the beginning)

--- Python/Module 03/exercise5.py
+++ Python/Module 03/exercise5.py
@@ -1,10 +1,10 @@
-talents = input ("Enter talents: ")
-pounds = input ("Enter pounds: ")
-lots = input ("Enter lots: ")
+talents = float(input ("Enter talents: "))
+pounds = float(input ("Enter pounds: "))
+lots = float(input ("Enter lots: "))
 
-talent = 20 * 32 * 13.3 * float(talents)
-pound = 32 * 13.3 * float(pounds)
-lot = 13.3 * float(lots)
+talent = 20 * 32 * 13.3 * talents
+pound = 32 * 13.3 * pounds
+lot = 13.3 * lots
 
 total_grams= lot + pound + talent
 kilograms= total_grams / 1000
@@ -14,5 +14,4 @@ print ("The weight in modern units:")
 print (str(int(kilograms)) + f" kilograms and {remaining_grams:.2f} grams.")
 
 # Note to myself: Remember input will always be a string so you must convert it to make calculations. And then, when printing, if mixed with strings, you must covert it back.
-# ASK IN CLASS ANYWAY - Note to myself: The output formatting does not work directly if you try to apply it to a str(remaining_grams), you must add it within a string and {remaining_grams:.2f}.
-# print (f"{remaining_grams:.2f}") works
+# Note to myself: The output formatting does not work directly if you try to apply it to a str(remaining_grams), you must add it within a string and {remaining_grams:.2f}. But this works: print (f"{remaining_grams:.2f}")
