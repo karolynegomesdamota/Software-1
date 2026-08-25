db4318f06983121263b11258a17b616c2fe015fe
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-25 15:24:23 +0300

    Corrected typo in mod03-ex5 and added mod04-ex1

--- Python/Module 03/exercise5.py
+++ Python/Module 03/exercise5.py
@@ -13,5 +13,5 @@ remaining_grams= (kilograms - int(kilograms)) * 1000
 print ("The weight in modern units:")
 print (str(int(kilograms)) + f" kilograms and {remaining_grams:.2f} grams.")
 
-# Note to myself: Remember input will always be a string so you must convert it to make calculations. And then, when printing, if mixed with strings, you must covert it back.
+# Note to myself: Remember input will always be a string so you must convert it to make calculations. And then, when printing, if mixed with strings, you must convert it back.
 # Note to myself: The output formatting does not work directly if you try to apply it to a str(remaining_grams), you must add it within a string and {remaining_grams:.2f}. But this works: print (f"{remaining_grams:.2f}")
