9c325645c98164223a8babef66ec16860aa6c820
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-26 09:44:11 +0300

    Code from class until the first break

---
+++ Python/Code examples from class/26/08/2026/loop_structure.py
@@ -0,0 +1,67 @@
+
+rounds = 3
+while rounds >= 0:
+    print("Hi, this is round " + str(rounds))
+    rounds = rounds - 1
+
+# Normal loop
+
+rounds = 5
+finished_rounds = 0
+while finished_rounds < rounds:
+    print ("Greeting!")
+    finished_rounds += 1
+
+# Fixed amount of repetition
+# finished_rounds += 1 means finished_rounds = finished_rounds + 1
+
+command = input ("What do you want do to? Enter exit to stop. ")
+
+while command != "exit":
+    print (f"You want to do {command}")
+    command = input ("What do you want to do? Enter exit to stop. ")
+
+# User ends repetition with command.
+
+counter = 1
+while counter <= 3:
+    print (counter)
+    counter = counter + 1
+
+# This is an example of exam question, where they ask what the result would be. Answer: 1,2,3.
+
+counter = 0
+while counter < 5:
+    print (counter)
+    counter = counter + 2
+
+# This is an example of exam question, where they ask what the result would be. Answer: 0,2,4.
+
+counter = 5
+while counter >= 0:
+    print (counter)
+    counter = counter - 1
+
+# This is an example of exam question, where they ask what the result would be. Answer: 3,2,1,0.
+
+outer = 1
+
+while outer <= 5:
+    inner = 1
+    while inner <= 5:
+        product = outer * inner
+        print(f"{outer} times {inner} is {product}")
+        inner += 1
+    outer += 1
+
+# Nested loops. We run 1 time the outer and inside of it the inner one will run 5 times. When this is finished, we will add one to the outer and run again the loop.
+
+command = input ("What do you want do to? ")
+
+while command != "stop":
+    if command =="help":
+        break
+    print (f"You want to do {command}")
+    command = input ("What do you want to do? ")
+
+# Break
