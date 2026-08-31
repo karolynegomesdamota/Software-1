849f0cc262ce030b131af47880d7408c00fa7bfa
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-08-31 13:05:39 +0300

    Project 2 - Main menu

--- Python/Project/program.py
+++ Python/Project/program.py
@@ -1,5 +1,24 @@
 player_name = input ("Enter your name: ")
-player_age = input ("Enter your age: ")
+player_age = int(input ("Enter your age: "))
 
 print(f"The name of the player is: {player_name}")
 print(f"The age of the player is: {player_age}")
+
+if player_age < 12:
+    print ("You are a minor!")
+else:
+    print (f"Welcome, {player_name}!")
+    print ("Main menu: ")
+    command = input ("Write a command or type 'lopeta' to exit. \n")
+    while command != "lopeta":
+        if command == "dance":
+            print("💃")
+        elif command == "eat":
+            print("😋")
+        elif command == "run":
+            print("🏃")
+        else:
+            print("Command not found.")
+
+        print ("Main menu: ")
+        command = input ("Write a command or type 'lopeta' to exit. \n")
