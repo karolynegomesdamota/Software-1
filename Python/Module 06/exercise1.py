e8d2ebb69c512c9ee903b802af7007eb6f4bbbaf
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-01 14:22:02 +0300

    Deleted some note for myself in MOD06-EX01

--- Python/Module 06/exercise1.py
+++ Python/Module 06/exercise1.py
@@ -8,5 +8,3 @@ for i in range(roll):
     results.append(random.randint(1,6))
 
 print (f"Sum of the dice: {sum(results)}")
-
-# Note for myself: Still pending to completely understand what i is in for. Ask in class.
