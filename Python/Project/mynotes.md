1a13f1a7802482f2b7e2f2082507044d4b6ac565
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-03 12:15:44 +0300

    Project 3 completed and created notes for myself

---
+++ Python/Project/mynotes.md
@@ -0,0 +1,13 @@
+Project 1:
+
+No problems
+
+Project 2:
+
+No problems
+
+Project 3:
+
+I was having an issue with my code where I was changing the energy, but afterwards when I printed it, the result was still 0.
+
+I asked the teacher and he explained I should add 'global variable' to the function where I was doing this.
--- Python/Project/program.py
+++ Python/Project/program.py
@@ -4,21 +4,72 @@ player_age = int(input ("Enter your age: "))
 print(f"The name of the player is: {player_name}")
 print(f"The age of the player is: {player_age}")
 
+# Backpack functions
+
+items = []
+
+def print_backpack ():
+    print(f"These are all the items in your backpack: {items}")
+
+def add_backpack ():
+    item = input ("Add an item to your backpack: ")
+    items.append(item)
+
+def remove_backpack ():
+    item = input ("Remove an item from your backpack: ")
+    items.remove(item)
+
+def choose_action_backpack ():
+    action = input("Type 'add' or 'remove': ")
+    if action == "add":
+        add_backpack ()
+    elif action == "remove":
+        remove_backpack ()
+    else:
+        print("Error! Start over.")
+
+# Energy functions
+
+energy = 0
+
+def print_energy ():
+    print(f"This is your energy: {energy}")
+
+def choose_action_energy ():
+    action = input("Type 'sleep' or 'exit': ")
+    if action == "sleep":
+        sleep()
+        print_energy ()
+    elif action == "exit":
+        print("Exiting energy tab.")
+
+def sleep ():
+    global energy
+    if energy < 100:
+        while energy < 100:
+            energy = energy + 20
+            print(f"Increasing +20 energy: {energy}")
+    else:
+        print("You have enough energy.")
+
+# Main program
+
 if player_age < 12:
     print ("You are a minor!")
 else:
     print (f"Welcome, {player_name}!")
     print ("Main menu: ")
-    command = input ("Write a command or type 'lopeta' to exit. \n")
-    while command != "lopeta":
-        if command == "dance":
-            print("💃")
-        elif command == "eat":
-            print("😋")
-        elif command == "run":
-            print("🏃")
+    command = input ("Choose a command: \n1 - backpack \n2 - energy \n3 - lopeta \n")
+    while command != "3":
+        if command == "1":
+            print_backpack ()
+            choose_action_backpack ()
+            print_backpack ()
+        elif command == "2":
+            print_energy ()
+            choose_action_energy ()
         else:
             print("Command not found.")
 
         print ("Main menu: ")
-        command = input ("Write a command or type 'lopeta' to exit. \n")
+        command = input ("Choose a command: \n1 - backpack \n2 - energy \n3 - lopeta \n")
