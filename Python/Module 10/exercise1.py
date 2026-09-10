ea4ef62c49f928f4b0732b78a4116183b066241f
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-10 19:17:22 +0300

    MOD10-EX01 completed and readme updated

---
+++ Python/Module 10/exercise1.py
@@ -0,0 +1,29 @@
+class Elevator:
+    def __init__(self, min_floor, max_floor):
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.floor = min_floor
+
+    def go_to_floor(self, number_floor):
+        self.number_floor = number_floor
+        if self.floor < self.number_floor and self.number_floor <= self.max_floor:
+            self.floor_up()
+        elif self.floor > self.number_floor and self.number_floor >= self.min_floor:
+            self.floor_down()
+        else:
+            print(f"The floor {number_floor} does not exist!")
+
+    def floor_up(self):
+        self.floor = self.floor + (self.number_floor - self.floor)
+        print(f"The current floor is {self.floor}")
+    def floor_down(self):
+        self.floor = self.floor - (self.floor - self.number_floor)
+        print(f"The current floor is {self.floor}")
+
+
+h = Elevator(1, 10)
+print("Basic elevator test:")
+h.go_to_floor(5)
+h.go_to_floor(1)
+
+# Code working correctly, but Moodle does not accept it.
