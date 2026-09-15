122882c3224dd3af3ab469d788466788a475cecd
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-15 20:12:40 +0300

    Changes to match naming required in EX02

--- Python/Module 10/exercise1.py
+++ Python/Module 10/exercise1.py
@@ -1,25 +1,25 @@
 class Elevator:
-    def __init__(self, min_floor, max_floor):
-        self.min_floor = min_floor
-        self.max_floor = max_floor
-        self.floor = min_floor
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
 
-    def go_to_floor(self, number_floor):
-        self.number_floor = number_floor                                                             #Is this the correct way of doing this? I had to add this here, in order for it to work.
-        if self.floor < self.number_floor and self.number_floor <= self.max_floor:
+    def go_to_floor(self, destination):
+        self.destination = destination
+        if self.current_floor < self.destination and self.destination <= self.top_floor:
             self.floor_up()
-        elif self.floor > self.number_floor and self.number_floor >= self.min_floor:
+        elif self.current_floor > self.destination and self.destination >= self.bottom_floor:
             self.floor_down()
         else:
-            print(f"The floor {number_floor} does not exist!")
+            print(f"The floor {destination} does not exist!")
 
     def floor_up(self):
-        self.floor = self.floor + (self.number_floor - self.floor)
-        print(f"The current floor is {self.floor}")
-    def floor_down(self):
-        self.floor = self.floor - (self.floor - self.number_floor)
-        print(f"The current floor is {self.floor}")
+        self.current_floor = self.current_floor + (self.destination - self.current_floor)
+        print(f"The current floor is {self.current_floor}")
 
+    def floor_down(self):
+        self.current_floor = self.current_floor - (self.current_floor - self.destination)
+        print(f"The current floor is {self.current_floor}")
 
 h = Elevator(1, 10)
 print("Basic elevator test:")
