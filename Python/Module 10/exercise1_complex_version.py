cf61d0631dd73d51f9ecd85b83c24f4becfe01b7
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-16 11:55:24 +0300

    Modifications to match Moodle

--- Python/Module 10/exercise1_complex_version.py
+++ Python/Module 10/exercise1_complex_version.py
@@ -7,33 +7,30 @@ class Elevator:
         self.current_floor = bottom_floor
 
     def go_to_floor(self, destination):
-        self.destination = destination
-        if self.current_floor < self.destination and self.destination <= self.top_floor:
-            self.floor_up()
-        elif self.current_floor > self.destination and self.destination >= self.bottom_floor:
-            self.floor_down()
+        print(f"\nCurrent floor: {self.current_floor}. Destination: {destination}.")
+        if self.current_floor < destination and destination <= self.top_floor:
+            while self.current_floor != destination:
+                self.floor_up()
+            print(f"We have reached floor {self.current_floor}!")
+        elif self.current_floor > destination and destination >= self.bottom_floor:
+            while self.current_floor != destination:
+                self.floor_down()
+            print(f"We have reached floor {self.current_floor}!")
         else:
             print(f"The floor {destination} does not exist!")
 
     def floor_up(self):
-        print(f"\nCurrent floor: {self.current_floor}. Destination: {self.destination}.")
-        while self.current_floor < self.destination:
-            self.current_floor += 1
-            print(f"Going up... {self.current_floor}")
-        self.current_floor = self.current_floor + (self.destination - self.current_floor)
-        print(f"We have reached floor {self.current_floor}!")
+        self.current_floor = self.current_floor + 1
+        print(f"Going up... {self.current_floor}")
 
     def floor_down(self):
-        print(f"\nCurrent floor: {self.current_floor}. Destination: {self.destination}.")
-        while self.current_floor > self.destination:
-            self.current_floor -= 1
-            print(f"Going down... {self.current_floor}")
-        self.current_floor = self.current_floor - (self.current_floor - self.destination)
-        print(f"We have reached floor {self.current_floor}!")
+        self.current_floor = self.current_floor - 1
+        print(f"Going down... {self.current_floor}")
 
 h = Elevator(1, 10)
 print("\nBasic elevator test:")
 h.go_to_floor(5)
 h.go_to_floor(1)
 
-# Code working correctly, but Moodle does not accept it.
+# Modifications done to match the simple version logic.
+# Now I make the elevator move one by one when it goes up and down. And within the method go_to_floor, I loop it over and over until we reach the destination.
