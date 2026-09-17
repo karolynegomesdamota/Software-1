64a854e257f0268c0c99cc1f90b464bed9afcbb0
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-17 11:54:59 +0300

    Changes made for Moodle to accept it

--- Python/Module 10/exercise2.py
+++ Python/Module 10/exercise2.py
@@ -5,39 +5,40 @@ class Elevator:
         self.current_floor = bottom_floor
 
     def go_to_floor(self, destination):
-        self.destination = destination
-        if self.current_floor < self.destination and self.destination <= self.top_floor:
-            self.floor_up()
-        elif self.current_floor > self.destination and self.destination >= self.bottom_floor:
-            self.floor_down()
+        if self.current_floor < destination and destination <= self.top_floor:
+            while self.current_floor != destination:
+                self.floor_up()
+            print(f"We have reached floor {self.current_floor}!")
+        elif self.current_floor > destination and destination >= self.bottom_floor:
+            while self.current_floor != destination:
+                self.floor_down()
+            print(f"We have reached floor {self.current_floor}!")
         else:
             print(f"The floor {destination} does not exist!")
 
     def floor_up(self):
-        self.current_floor = self.current_floor + (self.destination - self.current_floor)
-        print(f"We have reached floor {self.current_floor}!")
+        self.current_floor = self.current_floor + 1
 
     def floor_down(self):
-        self.current_floor = self.current_floor - (self.current_floor - self.destination)
-        print(f"We have reached floor {self.current_floor}!")
+        self.current_floor = self.current_floor - 1
 
 class Building:
     def __init__(self, bottom_floor, top_floor, amount_created_elevators):
-        self.list_of_elevators = []
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.elevators = []
         for i in range(amount_created_elevators):
-            i = Elevator(bottom_floor, top_floor)
-            self.list_of_elevators.append(i)
+            i = Elevator(self.bottom_floor, self.top_floor)
+            self.elevators.append(i)
 
     def run_elevator(self, number_of_elevator, destination):
         print(f"\nI'm currently running the elevator number {number_of_elevator}!")
-        self.list_of_elevators[number_of_elevator-1].go_to_floor(destination)
+        self.elevators[number_of_elevator].go_to_floor(destination)
 
 building = Building(1, 10, 3)
 
+building.run_elevator(0, 5)
+building.run_elevator(0, 2)
 building.run_elevator(1, 5)
-building.run_elevator(1, 2)
-building.run_elevator(2, 5)
-building.run_elevator(3, 2)
-building.run_elevator(1, 7)
-
-# Code working correctly, but Moodle does not accept it.
+building.run_elevator(2, 2)
+building.run_elevator(0, 7)
