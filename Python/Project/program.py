110523695e6182aae2e0437f8942959eccd925a2
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-21 20:15:40 +0300

    Added character class and used it in main prog

--- Python/Project/program.py
+++ Python/Project/program.py
@@ -52,12 +52,46 @@ def sleep ():
     else:
         print("You have enough energy.")
 
+
+# Classes
+
+# Class characters
+
+class Character:
+    def __init__(self, name, items, money, energy):
+        self.name = name
+        self.items = items
+        self.money = money
+        self.energy = energy
+
+character1 = Character("Cris", 10, 10, 10)
+character2 = Character("Toti", 5, 5, 5)
+character3 = Character("Bal", 2, 2, 2)
+
 # Main program
 
 if player_age < 12:
     print ("\nYou are a minor!")
 else:
     print (f"\nWelcome, {player_name}!")
+    print (f"\nChoose your character:")
+    print (f"\nPlease note this cannot be changed later on!")
+    choose_character = int(input(f"\n 1. {character1.name} with {character1.items} in bag, {character1.money} euros and {character1.energy} of energy. \n 2. {character2.name} with {character2.items} in bag, {character2.money} euros and {character2.energy} of energy. \n 3. {character3.name} with {character3.items} in bag, {character3.money} euros and {character3.energy} of energy.\n\n"))
+    while choose_character != 1 or 2 or 3:
+        if choose_character == 1:
+            print(f"\nYou chose {character1.name}!")
+            break
+        elif choose_character == 2:
+            print(f"\nYou chose {character2.name}!")
+            break
+        elif choose_character == 3:
+            print(f"\nYou chose {character3.name}!")
+            break
+        else:
+            print("\nError! Choose one of the options.")
+            choose_character = int(input(f"\n 1. {character1.name} with {character1.items} in bag, {character1.money} euros and {character1.energy} of energy. \n 2. {character2.name} with {character2.items} in bag, {character2.money} euros and {character2.energy} of energy. \n 3. {character3.name} with {character3.items} in bag, {character3.money} euros and {character3.energy} of energy.\n\n"))
+
+
     print ("\nMain menu: ")
     command = input ("Choose a command: \n1 - backpack \n2 - energy \n3 - lopeta \n")
     while command != "3":
