08e5de83877d2b6f1116e2036aaa4c90a0e2e9a9
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-21 19:11:34 +0300

    MOD11-EX02 completed and readme updated

---
+++ Python/Module 11/exercise2.py
@@ -0,0 +1,55 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate (self, change_of_speed):
+        if change_of_speed > 0:
+            for _ in range(change_of_speed):
+                if self.current_speed < self.maximum_speed:
+                    self.current_speed = self.current_speed + 1
+        elif change_of_speed < 0:
+            for _ in range(-1 * change_of_speed):
+                if self.current_speed > 0:
+                    self.current_speed = self.current_speed - 1
+
+    def drive(self, number_of_hours):
+        self.travelled_distance = self.travelled_distance + (number_of_hours * self.current_speed)
+
+class ElectricCar(Car):
+    def __init__(self, license_plate, maximum_speed, battery_capacity):
+        super().__init__(license_plate, maximum_speed)
+        self.battery_capacity = battery_capacity
+    pass
+
+class GasolineCar(Car):
+    def __init__(self, license_plate, maximum_speed, tank_volume):
+        super().__init__(license_plate, maximum_speed)
+        self.tank_volume = tank_volume
+    pass
+
+# Test code from Moodle:
+
+car = Car("TEST-123", 150)
+print(f"Car created: {car.license_plate}, max speed: {car.maximum_speed}")
+
+electric = ElectricCar("ELEC-123", 200, 75.0)
+print(f"Electric car: {electric.license_plate}, max speed: {electric.maximum_speed}, battery: {electric.battery_capacity} kWh")
+
+gasoline = GasolineCar("GAS-123", 180, 50.0)
+print(f"Gasoline car: {gasoline.license_plate}, max speed: {gasoline.maximum_speed}, tank: {gasoline.tank_volume} l")
+
+electric.accelerate(100)
+gasoline.accelerate(80)
+print(f"After acceleration - Electric: {electric.current_speed} km/h, Gasoline: {gasoline.current_speed} km/h")
+
+electric.drive(2)
+gasoline.drive(2)
+print(f"After 2 hours - Electric: {electric.travelled_distance} km, Gasoline: {gasoline.travelled_distance} km")
+
+
+"""
+Note for myself: I was having issues with the code because I was trying to pass also self inside the super().
+"""
--- README.md
+++ README.md
@@ -40,7 +40,7 @@ I completed exercise 1, 2, 3 and 4.
 
 ## Module 11
 
-I completed exercise 1.
+I completed exercise 1 and 2.
 
 ## Module 12
 
