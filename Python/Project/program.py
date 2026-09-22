4218c1bef0a46bb8044d89c3202d531c028fe938
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-22 13:34:50 +0300

    Desert and mountain functions

--- Python/Project/program.py
+++ Python/Project/program.py
@@ -57,7 +57,7 @@ def sleep ():
 # Boat lore:
 
 def lore_boat():
-    print(f"\nIf you choose to go this way, you will need a boat to get through the the lake.")
+    print(f"\nIf you choose to go this way, you will need a boat to get through the lake.")
     print(f"In order for you to build the boat, you will need to get wood. You will need 10 woods to build it.")
     print(f"You can get wood from the wood, but for each tree to cut, you will need to plant 2 seeds.")
     print(f"Check your bag to see how many seeds you have. If you do not have enough, you can buy more seed from the kiosk.")
@@ -68,7 +68,7 @@ def lore_boat():
 
 def check_wood():
 
-    boat = int(input("\nPress 1 to build the boat: \n1 - Build boat\n\n"))
+    boat = int(input("\nPress 1 to build the boat: \n1 - Build boat.\n\n"))
     if boat == 1:
         if items["wood"] >= 10:
             build_boat()
@@ -86,12 +86,60 @@ def build_boat():
         items["boat"] = 1
         print (f"\nYou now have " + str(items["wood"]) + " of wood and " + str(items["boat"]) + " boat!")
 
+# Desert functions:
+
+# Boat lore:
+
+def lore_desert():
+    print(f"\nIf you choose to go this way, you will need a water to get through the desert.")
+    print(f"In order for you to get water, you will need to pump it from the well. You will need 10 water to cross the desert.")
+    print(f"Check your bag to see how much water you have. If you do not have enough, you can buy more water from the kiosk.")
+
+    print(f"Once you have water enough, come back here to start your journey.")
+
+# Check water
+
+def check_water():
+
+    start_desert = int(input("\nPress 1 to start your journey: \n1 - Go through the desert.\n\n"))
+    if start_desert == 1:
+        if items["water"] >= 10:
+            goal()
+        else:
+            print("You don't have enough water! Go pump some from the well!")
+    else:
+        print("Error!")
+
+
 # Goal functions:
 
 def goal():
     print("\nYou now have everything needed! Let's go home!")
     print(3 * "\nLoading...\n")
     print("Congratulations! You got home!")
+
+# Freezing mountain functions:
+
+# Freezing mountain lore:
+
+def lore_freezing_mountain():
+    print(f"\nIf you choose to go this way, you will need a jacket to get through the freezing mountains.")
+    print(f"In order for you to get the jacket, you will need to get the jacket from the second hand store. You will need 10 euros to buy it.")
+    print(f"You can get money by trading items you have, but for each seed you have, you will get 1 euro.")
+    print(f"Check your panel to see how much money you have. If you do not have enough, you can trade some items in the trade-in store.")
+
+    print(f"Once you have a jacket, come back here to start your journey.")
+
+def check_jacket():
+
+    start_mountain = int(input("\nPress 1 to start your journey: \n1 - Go through the mountain.\n\n"))
+    if start_mountain == 1:
+        if items["jacket"] >= 1:
+            goal()
+        else:
+            print("You don't have a jacket! Go buy one!")
+    else:
+        print("Error!")
 
 # Classes
 
@@ -120,7 +168,7 @@ else:
     while choose_character != 1 or 2 or 3:
         if choose_character == 1:
             print(f"\nYou chose {character1.name}!")
-            items = {"seeds":character1.items, "money":character1.money, "energy":character1.energy, "wood":10, "water":0, "jacket":0}
+            items = {"seeds":character1.items, "money":character1.money, "energy":character1.energy, "wood":10, "water":10, "jacket":1}
             break
         elif choose_character == 2:
             print(f"\nYou chose {character2.name}!")
@@ -146,16 +194,13 @@ while command != 4:
             check_wood()
             break
         elif way_home == 2:
-            print(f"\nYou chose {character2.name}!")
+            lore_desert()
+            check_water()
             break
         elif way_home == 3:
-            print(f"\nYou chose {character3.name}!")
+            lore_freezing_mountain()
+            check_jacket()
             break
-        # Open the 3 options to get to the goal and explain what is needed for each one.
-        print("\nThese are the 3 ways to get home:")
-        print("1 - Lake. You need to build boat. To build boat, you need wood.")
-        print("2 - Desert. You need water. To get water, you have to pump it from the well.")
-        print("3 - Freezing mountain. You need a jack. To get a jacket, you need to buy it from the second hand store.")
     elif command == 2:
         # Open "panel" to show what is in the characters bag and also its energy.
         print("\nYour panel info:")
