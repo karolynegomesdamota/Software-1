c95b4fa7746eac4caf5b551276d80b1ffdd04f80
Karolyne Gomes da Mota <karolynegomesdamota@gmail.com>
2026-09-26 23:15:59 +0300

    Desert module

--- Python/Project/program.py
+++ Python/Project/program.py
@@ -5,39 +5,13 @@ from Goal import goal
 from Print_CommandNotFound import print_command_not_found
 from Print_Panel import print_panel
 from Lake import lore_boat, check_wood
+from Desert import lore_desert, check_water
 
 player_name = input ("\nEnter your name: ")
 player_age = int(input ("Enter your age: "))
 
 print(f"\nThe name of the player is: {player_name}")
 print(f"The age of the player is: {player_age}")
-
-# Desert functions:
-
-# Desert lore:
-
-def lore_desert():
-    print(f"\nIf you choose to go this way, you will need a water to get through the desert.")
-    print(f"In order for you to get water, you will need to pump it from the well. You will need 10 liters of water to cross the desert and the farmer who owns the well will charge you 1 coin per liter.")
-    print(f"Check your bag to see how much money you have. If you do not have enough, you can trade some items in the trade-in store.")
-
-    print(f"Once you have enough water, come back here to start your journey.")
-
-# Check water
-
-def check_water():
-
-    start_desert = int(input("\nPress 1 to start your journey: \n1 - Go through the desert.\n2 - Go back\n\n"))
-    if start_desert == 1:
-        if items["water"] >= 10:
-            goal()
-        else:
-            print("\nYou don't have enough water! Go pump some from the well!")
-    elif start_desert == 2:
-        way_home = way_home_menu() # This to make the way home menu to show and ask for a new option
-        return way_home # This to pass the new option inside the already first chosen option.
-    else:
-        print_command_not_found()
 
 # Freezing mountain functions:
 
